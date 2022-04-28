dcac350633d121292f2dca4f40045ec687638083
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-04-28 12:39:25 -0700

    add exercise 1

---
+++ exercise1.js
@@ -0,0 +1,26 @@
+"use strict";
+exports.__esModule = true;
+exports.logPerson = exports.users = void 0;
+;
+exports.users = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+function logPerson(user) {
+    console.log(" - ".concat(user.name, ", ").concat(user.age));
+}
+exports.logPerson = logPerson;
+console.log('Users:');
+exports.users.forEach(logPerson);
+/* In case if you are stuck:
+
+// https://www.typescriptlang.org/docs/handbook/2/objects.html
+*/
